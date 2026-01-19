8967a9bb92e0a106baa9f2aae162afeaf09c7fc4
Siti Jamilah Maghfiroh <jamilahmaghfiroh>
2026-01-19 07:50:02 +0700

    update tugas checkout

--- app/(landing)/components/layouts/footer.tsx
+++ app/(landing)/components/layouts/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer className="bg-dark-alternate text-white mt52">
+        <footer className="bg-dark-alternate text-white mt-52">
             <div className="container mx-auto flex justify-between pt-14 pb-24">
                 <div className="w-105">
                     <Image 
