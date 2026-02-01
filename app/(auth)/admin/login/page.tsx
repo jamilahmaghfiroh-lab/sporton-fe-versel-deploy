535c2aa1f24de326c704a85aef7a9c9626ddf837
Siti Jamilah Maghfiroh <jamilahmaghfiroh>
2026-02-01 07:36:06 +0700

    to fix the problem

--- app/(auth)/admin/login/page.tsx
+++ app/(auth)/admin/login/page.tsx
@@ -1,12 +1,42 @@
 "use client";
 
 import Button from "@/app/(landing)/components/ui/button";
+import { login } from "@/app/services/auth.service";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 
 const LoginPage = () => {
-    const {push} = useRouter()
+    const router = useRouter();
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            router.push("/admin/produtcs");
+        }
+    }, [router]);
+
+    const handleLogin = async () => {
+        setIsLoading(true)
+        try {
+            const data = await login({email, password});
+
+            if (data.token) {
+                router.push("/admin/products")
+            }
+        } catch(err: any){
+            setErrorMessage(err.message || "Something went wrong, please try again later.")
+            console.error("Login error", err)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+ 
     return (
         <main className="bg-[#F7F9FA] w-full min-h-screen flex justify-center items-center">
             <div className="max-w-136 w-full bg-white rounded-xl border-t-4 border-primary py-12 px-[72px]">
@@ -20,6 +50,13 @@ const LoginPage = () => {
                 <p className="opacity-50 text-sm text-center mb-9">
                     Enter your credentials to access the dashboard
                 </p>
+
+                {
+                    errorMessage && (
+                        <div className="px-3 py-1 bg-primary-light border border-primary rounded-md text-primary text-sm text-center w-full mb-2">
+                    {errorMessage}
+                </div>
+                )}
                 <div className="input-group-admin mb-5">
                     <label htmlFor="email">Email</label>
                     <input 
@@ -28,6 +65,8 @@ const LoginPage = () => {
                         name="email" 
                         placeholder="Please type your email" 
                         className="rounded-lg!"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
                 <div className="input-group-admin mb-12">
@@ -38,11 +77,17 @@ const LoginPage = () => {
                         name="password" 
                         placeholder="••••••••••••••••••••" 
                         className="rounded-lg!"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
-                <Button className="w-full rounded-lg! mb-8" onClick={() => push("/admin/products")}
+                <Button 
+                    className="w-full rounded-lg! mb-8"
+                    onClick={handleLogin}
                 >
-                    Sign In
+                    {
+                        isLoading ? "Signing in ..." : "Sign In"
+                    }
                 </Button>
             </div>
         </main>
--- app/(dashboard)/components/layouts/sidebar.tsx
+++ app/(dashboard)/components/layouts/sidebar.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { logout } from "@/app/services/auth.service";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+
 import { FiBox, FiCreditCard, FiLayers, FiLogOut, FiShoppingCart } from "react-icons/fi";
 
 const Sidebar = () => {
-    const pathname = usePathname()
+    const pathname = usePathname();
+    const {push} = useRouter()
 
     const menuItems = [
         {
@@ -29,7 +32,13 @@ const Sidebar = () => {
             icon: FiCreditCard, 
             link: "/admin/bank-info"
         },
-    ]
+    ];
+
+    const handleLogout = () => {
+        logout();
+        push("/admin/login")
+
+    }
 
     return (
         <aside className="w-80 min-h-screen bg-white border-r border-gray-100 flex flex-col fixed left-0 top-0">
@@ -62,10 +71,12 @@ const Sidebar = () => {
                 )
                 })}
             </div>
-            <Link href="#" className="flex gap-3 font-medium py-3 px-4.5 mx-5 hover:bg-gray-100 duration-300 rounded-lg mt-auto mb-10">
+            <button className="flex cursor-pointer gap-3 font-medium py-3 px-4.5 mx-5 hover:bg-gray-100 duration-300 rounded-lg mt-auto mb-10"
+            onClick={handleLogout}
+            >
                 <FiLogOut size={24} />
                 LogOut
-            </Link>
+            </button>
         </aside>
     );
 };
